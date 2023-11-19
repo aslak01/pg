36acb27bf1b13f968f8c690ea55e7e5d810ddce1
as <aslak.bakkeland@medialaben.no>
2023-11-20 00:52:55 +0100

    fix

--- main.ts
+++ main.ts
@@ -1,9 +1,5 @@
 #!/usr/bin/env/ deno
 
-// import input from "@inquirer/input";
-// import select from "@inquirer/select";
-// import chalk from "chalk";
-// import { oraPromise } from "ora";
 import { Command } from "cliffy-command";
 import { Select } from "cliffy-select";
 
@@ -15,55 +11,24 @@ import { isTruthy } from "./utils.ts";
 const { args } = await new Command()
   .arguments("<input:string> [output:string]")
   .parse(Deno.args);
-const arg = args[0];
+const arg = args.join(" ");
 
-async function main(query: unknown) {
-  if (!isTruthy(arg) && typeof arg !== "string") {
+async function main(query: string) {
+  if (!isTruthy(query) && typeof query !== "string") {
     return 1;
   }
-  const data = await getData(arg);
+  const data = await getData(query);
   if (data && data.length) {
     const options = data.map(parseItem);
 
     const selection = await Select.prompt({
-      message: "pick",
+      message: "Pick",
       options,
     });
-    await copy(selection);
+    await copy(String(selection));
     console.log(selection);
     return 0;
   }
 }
 
 main(arg);
-
-//   if (data[0].info_hash.split("").some((s: string) => s !== "0")) {
-//     const parsed = data.map((i) => parseItem(i));
-//
-//     console.clear();
-//
-//     const selection = select({
-//       message: "Chose a distro",
-//       choices: parsed,
-//       pageSize: 20,
-//     });
-//
-//     const selected = await selection;
-//     if (selected) {
-//       console.log(selected);
-//       return;
-//     }
-//
-//     console.log("Copied", chalk.green(selected));
-//     console.log("Press enter to exit");
-//   } else {
-//     console.log("No results for", query);
-//     console.log("Press enter to exit");
-//   }
-// }
-
-// async function main() {
-//   await searchLoop();
-// }
-//
-// main();
